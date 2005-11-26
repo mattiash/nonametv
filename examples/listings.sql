3cd192c0e43dcacd53aa61d0a83f56d87a6e289e
Mattias Holmlund <mattias.git@holmlund.se>
2005-11-26 12:02:09 +0000

    Channel and category updates
    git-svn-id: https://svn.holmlund.se/nonametv/nonametv/trunk@359 d387084b-b2e9-0310-8d71-80038c70edc2

--- examples/listings.sql
+++ examples/listings.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: listings
 -- ------------------------------------------------------
--- Server version	4.1.11-Debian_4sarge1
+-- Server version	4.1.11-Debian_4sarge2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
