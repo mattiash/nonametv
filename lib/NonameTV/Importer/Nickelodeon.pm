66e7498560e1528ed686c6477315e872f052bd1d
Mattias Holmlund <mattias.git@holmlund.se>
2006-09-28 19:26:49 +0000

    New importer from Nickelodeon implemented with a lexer and parser.
    git-svn-id: https://svn.holmlund.se/nonametv/nonametv/trunk@459 d387084b-b2e9-0310-8d71-80038c70edc2

--- lib/NonameTV/Importer/Nickelodeon.pm
+++ lib/NonameTV/Importer/Nickelodeon.pm
@@ -3,29 +3,38 @@ package NonameTV::Importer::Nickelodeon;
 use strict;
 use warnings;
 
-use utf8;
-
 =pod
 
-Import data from Word-files delivered via e-mail. 
+Import data from Word-files delivered via e-mail. The parsing of the
+data relies only on the text-content of the document, not on the
+formatting.
 
 Features:
 
 =cut
 
+use utf8;
+
 use DateTime;
-use DateTime::Duration;
+use XML::LibXML;
 
-use HTML::TreeBuilder;
-use HTML::Entities; 
-
-use NonameTV qw/MyGet Wordfile2HtmlTree Htmlfile2HtmlTree norm/;
+use NonameTV qw/MyGet Wordfile2Xml Htmlfile2Xml norm/;
 use NonameTV::DataStore::Helper;
-use NonameTV::Log qw/info progress error logdie/;
+use NonameTV::DataStore::Updater;
+use NonameTV::Log qw/info progress error logdie 
+                     log_to_string log_to_string_result/;
 
 use NonameTV::Importer;
 
 use base 'NonameTV::Importer';
+
+# The lowest log-level to store in the batch entry.
+# DEBUG = 1
+# INFO = 2
+# PROGRESS = 3
+# ERROR = 4
+# FATAL = 5
+my $BATCH_LOG_LEVEL = 4;
 
 sub new 
 {
@@ -44,17 +53,20 @@ sub new
 
   while( my $data = $sth->fetchrow_hashref )
   {
-    $self->{channel_data}->{$data->{xmltvid}} = 
-                            { id => $data->{id}, };
+    $self->{channel_data}->{$data->{grabber_info}} = 
+                            { id => $data->{id},
+                              xmltvid => $data->{xmltvid} 
+                            };
   }
 
   $sth->finish;
 
-  $self->{OptionSpec} = [ qw/verbose/ ];
+  $self->{OptionSpec} = [ qw/force-update verbose/ ];
   $self->{OptionDefaults} = { 
+    'force-update' => 0,
     'verbose'      => 0,
   };
-
+  
   return $self;
 }
 
@@ -63,272 +75,366 @@ sub Import
   my $self = shift;
   my( $p ) = @_;
 
+  NonameTV::Log::verbose( $p->{verbose} );
+
   foreach my $file (@ARGV)
   {
-    progress( "Nickelodeon: Processing $file" );
+    progress(  "Nickelodeon: Processing $file" );
     $self->ImportFile( "", $file, $p );
   } 
 }
 
 sub ImportFile
 {
-  my $self= shift;
-  my ($contentname, $file, $p)= @_;
-  
-  my $xmltvid= "nickelodeon.se";
-  
-  my $channel_id= $self->{channel_data}->{$xmltvid}->{id};
-  
-  my $dsh= $self->{datastorehelper};
-  
-  my $tree;
-  if($file =~  m/\.doc$/i) {
-    $tree= &Wordfile2HtmlTree($file);
-  }
-  elsif($file =~ /\.html$/) {
-    $tree= &Htmlfile2HtmlTree($file);
-  }
-  else {
-    error( "Nickelodeon: Unknown extension \"$file\"" );
-    return 0;
-  }
-  
-  if(!defined $tree) {
-    error( "Nickelodeon: \"$file\" failed to parse" );
-    return 0;
-  }
-  
-  # Some statics used more then once
-  my $vard='VARDAGAR|HVERDAG|HVERDAGAR|WEEKDAYS';
-  my $helg='HELGER|WEEKENDS';
-  my %monthno= ( januari => 1, januar => 1, january => 1, 
-                 februari => 2, februar => 2, february => 2,
-                 mars => 3, marts => 3, march => 3, 
-                 april => 4, 
-                 maj => 5, mai => 5, may => 5, 
-                 juni => 6, june => 6, 
-                 july => 7, juli => 7, 
-                 augusti => 8 , august => 8, 
-                 september => 9 , 
-                 oktober => 10, october => 10, 
-                 november => 11, november => 11, 
-                 december => 12, desember => 12, );
-  my $month= join '|', keys %monthno;
-  my $oneday= DateTime::Duration->new(days => 1);
-  
-  # Lets find those schedules
-  for my $e ($tree->look_down('_tag', 'p',
-     sub { 
-       # Strict test
-       my $a = $_[0]->as_text() =~ 
-         m/^\s*($vard|$helg)\s*
-            ([0-9]{1,2})\s*
-            ($month)?\s*
-            (2[0-9]{3})?\s*
-            -\s*
-            ([0-9]{1,2})\s*
-            ($month)\s*
-            (2[0-9]{3})\s*
-            $/ix;
+  my $self = shift;
+  my( $contentname, $file, $p ) = @_;
 
-       # Loose test
-       my $b = $_[0]->as_text() =~ 
-         m/^\s*([a-zåäöÅÄÖ]+)\s*
-            ([0-9]{1,2})\s*
-            ([a-zåäöÅÄÖ]+)?\s*
-            (2[0-9]{3})?\s*
-            -\s*
-            ([0-9]{1,2})\s*
-            ([a-zåäöÅÄÖ]+)\s*
-            (2[0-9]{3})\s*
-            $/ix;
-       
-       if( $a != $b ) 
-       { 
-         error( "Nickelodeon: $file has ambiguous period: " . 
-                $_[0]->as_text() ); 
-       }
- 
-       return $a;
-     } ) )
-
+  my $doc;
+  if( $file =~  /doc$/i )
   {
-    my $startday= my $endday= undef;
-    #print $e->as_text(),"\n";
-    if($e->as_text() =~ m/([0-9][0-9]?)\s*($month)\s*(2[0-9]{3})\s*-\s*([0-9][0-9]?)\s*($month)\s*(2[0-9]{3})/i) 
-    {
-      # DD Month YYYY - DD Month YYYY 
-      # (haven't seen this but guess it would be possible)
-      eval 
-      { 
-        $startday = DateTime->new( year   => $3,
-                                   month  => $monthno{lc($2)},
-                                   day    => $1,
-                                   time_zone => 'Europe/Stockholm'); 
+    $doc = Wordfile2Xml( $file );
+  }
+  elsif( $file =~ /html$/i )
+  {
+    $doc = Htmlfile2Xml( $file );
+  }
+  else
+  {
+    error( "Nickelodeon: Unknown filename $file" );
+    return;
+  }
 
-        $endday= DateTime->new(year   => $6,
-                               month  => $monthno{lc($5)},
-                               day    => $4,
-                               time_zone => 'Europe/Stockholm'); 
-      }
-    } 
-    elsif($e->as_text() =~ m/([0-9][0-9]?)\s*($month)\s*-\s*([0-9][0-9]?)\s*($month)\s*(2[0-9]{3})/i) 
-    {
-      # DD Month - DD Month YYYY
-      eval 
-      { 
-        $startday= DateTime->new(year   => $5,
-                                 month  => $monthno{lc($2)},
-                                 day    => $1,
-                                 time_zone => 'Europe/Stockholm');
-        
-        $endday= DateTime->new(year   => $5,
-                               month  => $monthno{lc($4)},
-                               day    => $3,
-                               time_zone => 'Europe/Stockholm'); 
-      }
-    } 
-    elsif($e->as_text() =~ m/([0-9][0-9]?)\s*-\s*([0-9][0-9]?)\s*($month)\s*(2[0-9]{3})/i) 
-    {
-      # DD - DD Month YYYY
-      eval 
-      { 
-        $startday= DateTime->new(year   => $4,
-                                 month  => $monthno{lc($3)},
-                                 day    => $1,
-                                 time_zone => 'Europe/Stockholm');
+  if( not defined( $doc ) )
+  {
+    error( "Nickelodeon: Failed to parse $file" );
+    return;
+  }
 
-        $endday= DateTime->new(year   => $4,
-                               month  => $monthno{lc($3)},
-                               day    => $2,
-                               time_zone => 'Europe/Stockholm'); 
-      }
-    } 
-    else 
-    {
-      logdie "This should only happen if the regexps above are wrong!";
-    }
+  $self->ImportData( $file, $doc );
+}
+
+# Import files that contain full programming details,
+# usually for an entire month.
+# $doc is an XML::LibXML::Document object.
+sub ImportData
+{
+  my $self = shift;
+  my( $filename, $doc ) = @_;
+  
+  my $loghandle;
+
+  # Find all div-entries.
+  my $ns = $doc->find( "//div" );
+  
+  if( $ns->size() == 0 )
+  {
+    error( "Discovery: No programme entries found in $filename" );
+    return;
+  }
+  
+  # States
+  use constant {
+    ST_START  => 0,
+    ST_FDATE  => 1,   # Found date
+    ST_FHEAD  => 2,   # Found head with starttime and title
+    ST_FEND => 4,   # After END-marker
+  };
+  
+  use constant {
+    T_DATE => 10,
+    T_HEAD => 11,
+    T_TEXT => 12,
+    T_END => 13,
+  };
+  
+  # We keep track of the earliest and latest mentioned dates
+  # and make sure that we see data for each day between these dates
+  # exactly once.
+  $self->{earliest_date} = DateTime->today->add( days => 1000 );
+  $self->{latest_date} = DateTime->today->subtract( days => 1000 );
+  my %seen_days;
+
+  my $state=ST_START;
+
+  my( $fromdate, $todate, $weekdays );
+  my( $periodstart, $periodend, $periodweekdays, $periodtext );
+  my $entries = [];
+  my $start;
+  my $title;
+  
+  foreach my $div ($ns->get_nodelist)
+  {
+    my( $text ) = norm( $div->findvalue( './/text()' ) );
+    next if $text eq "";
+
+    my $type;
     
-    unless(defined($startday) && defined($endday)) 
+    if( $text =~ /^\d{1,2}(\.|:)\d{1,2}\s+(END)|(SLUT)\s*$/i ) {
+      $type = T_END;
+      ($start) = ($text =~ /^(\d+[\.:]\d+)\s+/ );
+      $start =~ tr/\./:/;
+    }
+    elsif( $text =~ /^\d{1,2}(\.|:)\d{1,2}\s+\S+/ )
     {
-      error('$file: WARNING! Invalid date in time period: "' .
-            &norm($e->as_text()) . "\" ... skipping!");
-      next;
+      $type = T_HEAD;
+      $start=undef;
+      $title=undef;
+      ($start, $title) = ($text =~ /^(\d+[\.:]\d+)\s+(.*?)\s*$/ );
+      $start =~ tr/\./:/;
+    }
+    elsif( match_date_range( $text, \$fromdate, \$todate, \$weekdays ) )
+    {
+      $type = T_DATE;
+    }
+    else {
+      $type = T_TEXT;
     }
 
-    #print $startday->ymd,' => ',$endday->ymd,"\n";
-    if($startday->compare($endday) > 0) 
-    {
-      error("$file: Start day (" . $startday->ymd('-') . 
-            ") is after End day (" . $endday->ymd('-') .
-            "), I'm adding a year to End day!");
-      $endday = $endday->add( years => 1 );
-    }
-
-    error( "$file: Does not recognize document structure. " . 
-           "Trying to get schedule anyway ..." )
-      unless $e->right()->attr('_tag') eq 'p';
-
-    my %sched= ();
-    my $title= my $time= undef;
-    my $descr= '';
-    for my $t ($e->right()) 
-    {
-      next unless ref($t) eq 'HTML::Element';
-      if($t->as_text() =~ m/^\s*([0-9]{2}):([0-9]{2})\s+(.*)\s*$/ &&
-         $1 < 24 && $2 < 60) {
-        if(defined $time) 
-        { 
-          # store previous entry when we find new start time
-          error( "$file: Overwriting exisiting entry (starttime=$time)!" )
-            if exists $sched{$time};
-
-          $sched{$time}{'title'}= $title;
-          $sched{$time}{'descr'}= $descr if length $descr;
-          #print "$time -- $title -- $descr\n\n";
-          $descr= '';
-        }
-
-        $time= "$1:$2";
-        $title= norm($3);
-        if($title =~ m/^End|Slut$/) 
-        {
-          $title= 'End';
-          $sched{$time}{'title'}= 'end-of-transmission';
-          last;
-        }
-      } 
-      else 
-      {
-        my $txt= norm($t->as_text());
-
-        if(lc($txt) eq 'end' && defined $time) 
-        { 
-          # Danish schedule have end-tag without time
-          error( "$file: Overwriting exisiting entry (starttime=$time)!" )
-            if exists $sched{$time};
-
-          $sched{$time}{'title'}= $title;
-          $sched{$time}{'descr'}= $descr if length $descr;
-          $title= 'End';
-          # We assume last program is 30 minutes long
-          (my $h, my $m)= split /:/, $time;
-          $m+= 30;
-          if($m >= 60) 
-          {
-            ++$h;
-            $m-= 60;
-          }
-          $time= sprintf("%02d:%02d", $h, $m);
-          error( "$file: End of transmission time not found using: $time" );
-          $sched{"$time"}{'title'}= 'end-of-transmission';
-          last;
-        }
-
-        $descr.= (length($descr) ? ' ' : '').$txt;
+#    print "$state $type\n";
+    if( $state == ST_START ) {
+      if( $type == T_DATE ) {
+        $periodstart = $fromdate;
+        $periodend = $todate;
+        $periodweekdays = $weekdays;
+        $periodtext = $text;
+        $state = ST_FDATE;
+      }
+      else {
+        error( "Nickelodeon: Unexpected text in state ST_START: $text" );
+        next;
+      }
+    } 
+    elsif( $state == ST_FDATE ) {
+      if( $type == T_HEAD ) {
+        push @{$entries}, [$start, $title, ""];
+        $state = ST_FHEAD;
+      }
+      else {
+        error( "Nickelodeon: Unexpected text in state ST_FDATE: $text" );
+        next;
       }
     }
-
-    unless(scalar(keys %sched)) 
-    {
-      error( "$file: Schedule not found ... skipping!" );
-      next;
+    elsif( $state == ST_FHEAD ) {
+      if( $type == T_DATE ) {
+        $self->process_entries( $periodstart, $periodend, $periodweekdays,
+                         $entries ) 
+          or error( "Nickelodeon: No matching days for $periodtext" );
+        $periodstart = $fromdate;
+        $periodend = $todate;
+        $periodweekdays = $weekdays;
+        $periodtext = $text;
+        $entries = [];
+        $state = ST_FDATE;
+      }
+      elsif( $type == T_HEAD ) {
+        push @{$entries}, [$start, $title, ""];
+        $state = ST_FHEAD;
+      }
+      elsif( $type == T_END ) {
+        push @{$entries}, [$start, "end-of-transmission", ""];
+        $self->process_entries( $periodstart, $periodend, $periodweekdays,
+                         $entries ) 
+          or error( "Nickelodeon: No matching days for $periodtext" );
+        $periodstart = undef;
+        $periodend = undef;
+        $periodweekdays = undef;
+        $periodtext = undef;
+        $entries = [];
+        $state = ST_FEND;
+      }
+      elsif( $type == T_TEXT ) {
+        $entries->[-1]->[2] .= " " . $text;
+      }
+      else {
+        error( "Nickelodeon: Unexpected text in state ST_FHEAD: $text" );
+        next;
+      }
     }
+    elsif( $state == ST_FEND ) {
+      if( $type == T_DATE ) {
+        $periodstart = $fromdate;
+        $periodend = $todate;
+        $periodweekdays = $weekdays;
+        $periodtext = $text;
+        $entries = [];
+        $state = ST_FDATE;
+      }
+      else {
+        error( "Nickelodeon: Unexpected text in state ST_FEND: $text" );
+        next;
+      }
+    }
+  }
+  $self->process_entries( $periodstart, $periodend, $periodweekdays,
+                   $entries )
+    unless $state == ST_FEND;
 
-    error( "$file: End of transmission not found!" )
-      unless $title eq 'End';
+  my $currdate = $self->{earliest_date}->clone();
 
-    # Got entire schedule and days, lets Import it!
-    my $weekdays= $e->as_text() =~ m/$vard/ ? '[1-5]' : '[67]';
-    while($startday->compare($endday) <= 0) 
-    {
-      next if $startday->wday() !~ m/^$weekdays$/;
-      $dsh->StartBatch(${xmltvid}.'_'.$startday->ymd('-'), $channel_id);
-      $dsh->StartDate($startday->ymd('-'));
-      for $time (sort keys(%sched)) 
-      {
-        my $ce = 
-        { 
-          title       => $sched{$time}{'title'},
-          start_time  => $time
+  while($currdate <= $self->{latest_date}) 
+  {
+    if( !defined($self->{seen_days}->{$currdate->ymd("-")} ) ) {
+      error( "No data for " . $currdate->ymd('-') );
+    }
+    $currdate = $currdate->add( days => 1 );
+  }
+}
+
+sub process_entries {
+  my $self = shift;
+  my( $startdate, $enddate, $weekdays, $entries ) = @_;
+
+  my $dsh= $self->{datastorehelper};
+  my $xmltvid= "nickelodeon.se";
+  my $channel_id= $self->{channel_data}->{$xmltvid}->{id};
+
+  $self->{earliest_date} = $startdate->clone()
+    if( $self->{earliest_date} > $startdate );
+
+  $self->{latest_date} = $enddate->clone()
+    if( $self->{latest_date} < $enddate );
+  
+  my $currdate = $startdate->clone();
+  my $matches = 0;
+
+  while($currdate <= $enddate) 
+  {
+    if( $currdate->wday() =~ m/$weekdays/ ) {
+#      print $currdate->ymd("-") . "\n";
+
+      error( "Nickelodeon: Duplicate data for " . $currdate->ymd('-') )
+        if( defined( $self->{seen_days}->{$currdate->ymd("-")} ) );
+
+      $self->{seen_days}->{$currdate->ymd("-")} = 1;
+      $matches++;
+
+      $dsh->StartBatch($xmltvid.'_'.$currdate->ymd('-'), $channel_id);
+      $dsh->StartDate($currdate->ymd('-'));
+      foreach my $entry (@{$entries}) {
+        my( $start, $title, $desc ) = @{$entry};
+        my $ce = {
+          start_time  => $start,
+          title       => norm( $title ),
         };
 
-        $ce->{'description'}= $sched{$time}{'descr'} 
-          if exists $sched{$time}{'descr'};
+        $ce->{description} = norm($desc) if $desc =~ /\S/;
 
         $dsh->AddProgramme($ce);
       }
       $dsh->EndBatch(1);
-    } 
-    continue 
-    {
-      $startday->add_duration($oneday);
+    }
+    $currdate = $currdate->add( days => 1 );
+  }
+
+  return $matches;
+}
+
+my @weekdays_sv = qw/måndag tisdag onsdag torsdag fredag lördag söndag/;
+
+my $WD = join( "|", @weekdays_sv, map { $_ . "ar" } @weekdays_sv );
+my $WD_SET = join( "|", "vardagar", "helger" );
+
+my %weekdayno = ( vardagar => "[12345]",
+                  helger => "[67]",
+                  );
+
+for( my $i=0; $i < scalar( @weekdays_sv ); $i++ ) 
+{ 
+  $weekdayno{$weekdays_sv[$i]} = $i+1;
+}
+
+sub match_date_range {
+  my( $text, $fromdateref, $todateref, $weekdaysref ) = @_;
+
+  return 0 unless $text =~ s/^\s*($WD|$WD_SET)\s*//i;
+
+  my $wd = $weekdayno{lc($1)};
+
+  my( $fromspec, $tospec ) = split( /\s*-\s*/, $text, 2 );
+
+  my $fromdate = parse_date( $fromspec, DateTime->today );
+  my $todate;
+
+  if( defined( $tospec ) ) {
+    $todate = parse_date( $tospec, $fromdate );
+  }
+  else { 
+    $todate = $fromdate->clone();
+  }
+
+  if( DateTime->today->subtract( months => 2 ) > $fromdate ) {
+    $fromdate = $fromdate->add( years => 1 );
+  }
+
+  if( $fromdate > $todate ) {
+    $todate = $todate->add( years => 1 );
+  }
+
+  $$fromdateref = $fromdate;
+  $$todateref = $todate;
+  $$weekdaysref = $wd;
+
+#  print "$wd " . $fromdate->ymd() . " " . $todate->ymd() . "\n";
+
+  return 1;
+}
+
+my %monthno= ( januari => 1, januar => 1, january => 1, 
+               februari => 2, februar => 2, february => 2,
+               mars => 3, marts => 3, march => 3, 
+               april => 4, 
+               maj => 5, mai => 5, may => 5, 
+               juni => 6, june => 6, 
+               july => 7, juli => 7, 
+               augusti => 8 , august => 8, 
+               september => 9 , 
+               oktober => 10, october => 10, 
+               november => 11, november => 11, 
+               december => 12, desember => 12, );
+
+# Parse a (partial) date into a proper DateTime object.
+# Takes two parameters: A string containing a partial
+# date specification and a DateTime object to use for
+# any components that are not specified by the date 
+# specification.
+sub parse_date {
+  my( $str, $def_dt ) = @_;
+
+  my $dt = $def_dt->clone();
+
+  # Avoid issues with days that don't exist in all months.
+  $dt->set( day => 1 );
+
+  my( $day, $month, $year ) = split( /\s+/, $str );
+
+  if( defined( $year ) ) {
+    if( $year !~ /^[0-9]{4}$/ ) {
+      error( "Nickelodeon: Unknown year $year\n" );
+    }
+    else {
+      $dt->set( year => $year );
     }
   }
 
-  $tree->delete;
-}
+  if( defined( $month ) ) {
+    if( not exists( $monthno{ $month } ) ) {
+      error( "Nickelodeon: Unknown month $month\n" );
+    }
+    else {
+      $dt->set( month => $monthno{$month} );
+    }
+  }
 
+  if( $day !~ /^[0-9]{1,2}$/ ) {
+    error( "Nickelodeon: Unknown day $day\n" );
+  }
+  else {
+    $dt->set( day => $day );
+  }
+
+  return $dt;
+}
+  
 1;
 
 ### Setup coding system
