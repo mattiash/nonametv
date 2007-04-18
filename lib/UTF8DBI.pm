899d1cbd54a2d0037f1fa946234893f88d8c1e63
Mattias Holmlund <mattias.git@holmlund.se>
2007-04-18 20:38:28 +0000

    Imported new version of UTF8DBI.pm from http://dysphoria.net/2006/02/05/utf-8-a-go-go/
    git-svn-id: https://svn.holmlund.se/nonametv/nonametv/trunk@581 d387084b-b2e9-0310-8d71-80038c70edc2

--- lib/UTF8DBI.pm
+++ lib/UTF8DBI.pm
@@ -1,25 +1,37 @@
 # UTF8DBI.pm re-implementation by Pavel Kudinov http://search.cpan.org/~kudinov/
 # originally from: http://dysphoria.net/code/perl-utf8/
+# And patched again by Andrew Forrest, Jan 2007
 
-package UTF8DBI    ; use base DBI    ;
-package UTF8DBI::db; use base DBI::db;
-package UTF8DBI::st; use base DBI::st;
+use DBI 1.21;
+use utf8;
+use Encode;
 
+package UTF8DBI; use base DBI;
 sub _utf8_ {
-  use Encode;
-  if    (ref $_ eq 'ARRAY'){ _utf8_() foreach        @$_  }
-  elsif (ref $_ eq 'HASH' ){ _utf8_() foreach values %$_  }
-  else                     {         Encode::_utf8_on($_) };
-  $_;
+  if    (ref $_ eq 'ARRAY') {_utf8_() foreach        @$_ }
+  elsif (ref $_ eq 'HASH' ) {_utf8_() foreach values %$_ }
+else { 
+Encode::_utf8_on($_);
+$_ = '⁂malformed-UTF8‼' #die "Malformed utf8 string in database"
+if (Encode::is_utf8($_) && ! Encode::is_utf8($_, 1));
+};
+$_;
 };
 
-sub fetch             { return _utf8_ for shift->SUPER::fetch            (@_)  };
-sub fetchrow_arrayref { return _utf8_ for shift->SUPER::fetchrow_arrayref(@_)  };
-sub fetchrow_hashref  { return _utf8_ for shift->SUPER::fetchrow_hashref (@_)  };
-sub fetchall_arrayref { return _utf8_ for shift->SUPER::fetchall_arrayref(@_)  };
-sub fetchall_hashref  { return _utf8_ for shift->SUPER::fetchall_hashref (@_)  };
-sub fetchcol_arrayref { return _utf8_ for shift->SUPER::fetchcol_arrayref(@_)  };
 
-sub fetchrow_array    {                 @{shift->       fetchrow_arrayref(@_)} };
+package UTF8DBI::db; use base DBI::db;
+
+sub selectrow_arrayref { return UTF8DBI::_utf8_ for shift->SUPER::selectrow_arrayref(@_) };
+sub selectrow_hashref  { return UTF8DBI::_utf8_ for shift->SUPER::selectrow_hashref (@_) };
+sub selectall_arrayref { return UTF8DBI::_utf8_ for shift->SUPER::selectall_arrayref(@_) };
+sub selectall_hashref  { return UTF8DBI::_utf8_ for shift->SUPER::selectall_hashref (@_) };
+sub selectcol_arrayref { return UTF8DBI::_utf8_ for shift->SUPER::selectcol_arrayref(@_) };
+
+sub selectrow_array    { @{shift->selectrow_arrayref(@_)} };
+
+
+package UTF8DBI::st; use base DBI::st;
+
+sub fetch              { return UTF8DBI::_utf8_ for shift->SUPER::fetch             (@_)  };
 
 1;
