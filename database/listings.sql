c9ee212a7d8b5b64638cca19ef9ede6a5218d45e
Mattias Holmlund <mattias.git@holmlund.se>
2007-05-30 19:41:45 +0000

    Remove comments from database dump.
    git-svn-id: https://svn.holmlund.se/nonametv/nonametv/trunk@607 d387084b-b2e9-0310-8d71-80038c70edc2

--- database/listings.sql
+++ database/listings.sql
@@ -1,24 +1,3 @@
--- MySQL dump 10.11
---
--- Host: localhost    Database: listings
--- ------------------------------------------------------
--- Server version	5.0.32-Debian_7etch1-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `batches`
---
-
 DROP TABLE IF EXISTS `batches`;
 CREATE TABLE `batches` (
   `id` int(11) NOT NULL auto_increment,
@@ -28,10 +7,6 @@ CREATE TABLE `batches` (
   `abort_message` text NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Table structure for table `channels`
---
 
 DROP TABLE IF EXISTS `channels`;
 CREATE TABLE `channels` (
@@ -51,10 +26,6 @@ CREATE TABLE `channels` (
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `files`
---
-
 DROP TABLE IF EXISTS `files`;
 CREATE TABLE `files` (
   `id` int(11) NOT NULL auto_increment,
@@ -67,10 +38,6 @@ CREATE TABLE `files` (
   `md5sum` varchar(33) NOT NULL default '',
   PRIMARY KEY  (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Table structure for table `programs`
---
 
 DROP TABLE IF EXISTS `programs`;
 CREATE TABLE `programs` (
@@ -100,20 +67,12 @@ CREATE TABLE `programs` (
   KEY `batch` (`batch_id`,`start_time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `state`
---
-
 DROP TABLE IF EXISTS `state`;
 CREATE TABLE `state` (
   `name` varchar(60) NOT NULL default '',
   `value` text,
   PRIMARY KEY  (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Table structure for table `trans_cat`
---
 
 DROP TABLE IF EXISTS `trans_cat`;
 CREATE TABLE `trans_cat` (
@@ -124,10 +83,6 @@ CREATE TABLE `trans_cat` (
   PRIMARY KEY  (`type`,`original`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `languagestrings`
---
-
 DROP TABLE IF EXISTS `languagestrings`;
 CREATE TABLE `languagestrings` (
   `module` varchar(32) NOT NULL default '',
@@ -135,14 +90,4 @@ CREATE TABLE `languagestrings` (
   `strvalue` text NOT NULL,
   `language` varchar(4) NOT NULL default ''
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2007-05-27 19:58:00
