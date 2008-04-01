eec97742dca45c5e995429a4e913be4555b32ace
Mattias Holmlund <mattias.git@holmlund.se>
2008-04-01 12:07:36 +0200

    Add override.conf support.

--- lib/NonameTV/Config.pm
+++ lib/NonameTV/Config.pm
@@ -1,5 +1,7 @@
 package NonameTV::Config;
 
+use strict;
+
 =head1 NAME
 
 NonameTV::Config - Read and parse the configuration file(s) for
@@ -13,10 +15,40 @@ NonameTV.
 
 =head1 DESCRIPTION
 
+NonameTV looks for the configuration file first at 
+
+    $HOME/.nonametv.conf
+
+and if that file is not found then it looks at
+
+    /etc/nonametv.conf
+
+After one of these files are loaded, it also looks for a file called override.conf in the root-directory of the NonameTV installation. The contents of override.conf will override any parameters set in the other configuration file.
+
+override.conf is meant to contain any parameters that are specific to
+this installation of NonameTV. This can be used to have one production version of NonameTV and one development version on the same system. The override.conf for the development-version can then contain:
+
+  {
+    DataStore => {
+      dbname => 'nonametv-dev',
+    },
+
+    Cache => {
+      BasePath => '/var/local/nonametv-dev/cache',
+    }
+
+    ContentCachePath => '/var/local/nonametv-dev/contentcache/',
+
+    FileStore => '/var/local/nonametv-dev/channels/',
+    LogFile => '/var/log/nonametv/nonametv-dev.log',
+  }
 
 =cut
 
 use File::Slurp;
+use Carp;
+
+use NonameTV::Path;
 
 BEGIN {
     use Exporter   ();
@@ -40,12 +72,12 @@ sub ReadConfig {
   return $Conf if defined $Conf;
 
   my $file;
-  if (-e "$HOME/.nonametv.conf") {
-  	$file = "$HOME/.nonametv.conf";
+  if (-e "$ENV{HOME}/.nonametv.conf") {
+  	$file = "$ENV{HOME}/.nonametv.conf";
   } elsif (-e "/etc/nonametv.conf") {
   	$file = "/etc/nonametv.conf";
   } else {
-  	die "No configuration file found in $HOME/.nonametv.conf or /etc/nonametv.conf"
+  	die "No configuration file found in $ENV{HOME}/.nonametv.conf or /etc/nonametv.conf"
   }
   
   my $config = read_file( $file );
@@ -53,8 +85,34 @@ sub ReadConfig {
   my $conf = eval( $config );
   die "Error in configuration file $file: $@" if $@;
 
+  my $override_file = NonameTV::Path::Root() . "/override.conf";
+  if( -f $override_file ) {
+    my $str = read_file( $override_file );
+    my $override = eval( $str );
+    die "Error in configuration file $override_file: $@" if $@;
+    mergehash( $conf, $override );
+  }
+
   $Conf = $conf;
   return $conf;
+}
+
+sub mergehash {
+  my( $src, $add ) = @_;
+
+  croak "$src is not a hashref" if ref( $src ) ne "HASH";
+  croak "$add is not a hashref" if ref( $add ) ne "HASH";
+
+  foreach my $key (keys %{$add}) {
+    if( not ref( $add->{$key} ) ) {
+      $src->{$key} = $add->{$key};
+    }
+    elsif( ref( $add->{$key} ) eq "HASH" ) {
+      $src->{$key} = {} if not defined $src->{$key};
+      mergehash( $src->{$key}, $add->{$key} );
+    }
+  }
+  
 }
 
 1;
