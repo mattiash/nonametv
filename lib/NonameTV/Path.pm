e297e03af377a3cb9471bf55a9a9d13305fd0a84
Mattias Holmlund <mattias.git@holmlund.se>
2008-04-14 21:19:32 +0200

    Improved method of finding the root dir.
    Removed the environment variable since it is no longer necessary.

--- lib/NonameTV/Path.pm
+++ lib/NonameTV/Path.pm
@@ -13,41 +13,20 @@ NonameTV::Path - Locate different parts of the NonameTV source.
 =head1 DESCRIPTION
 
 Locates the full path to the directory where NonameTV is located.
-This is done based on the location of the script that is running,
-which means that it will work even if there are several instances
-of NonameTV on your system.
-
-To override the location, set the environment variable NONAMETV_ROOT so
-that $NONAMETV_ROOT/lib/NonameTV/Path.pm exists.
+This is done based on the location of the NonameTV::Path module.
 
 =cut
 
-use FindBin;
 use Cwd qw/abs_path/;
 
 my $root;
 
 BEGIN {
-  if( defined( $ENV{NONAMETV_ROOT} ) ) {
-    $root = $ENV{NONAMETV_ROOT};
-    if( not -f "$root/lib/NonameTV/Path.pm" ) {
-      print STDERR "Failed to find NonameTV installation at $root.\n";
-      print STDERR "Please set the NONAMETV_ROOT environment variable correctly.\n";
-      exit 1;
-    }
-  }
-  else {
-    $root = abs_path( $FindBin::Bin );
-
-    while( not -f "$root/lib/NonameTV/Path.pm" ) {
-      my $oldroot = $root;
-      $root = abs_path( "$root/.." );
-      if( $root eq $oldroot ) {
-	print STDERR "NonameTV::Path Failed to find the NonameTV installation directory.\n";
-	print STDERR "Started at $FindBin::Bin/..\n";
-	exit 1;
-      }
-    }
+  my $pathfile = abs_path( $INC{"NonameTV/Path.pm"} );
+  ( $root ) = ( $pathfile =~ m%(.*)/lib/NonameTV/Path.pm% ); 
+  if( not -f "$root/lib/NonameTV/Path.pm" ) {
+    print STDERR "Failed to find NonameTV installation at $root.\n";
+    exit 1;
   }
 }
 
