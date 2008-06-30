47765e622e4629e47690c5f490885b357e9c8997
Tomica Crnek <tcrnek@gmail.com>
2008-07-01 01:14:50 +0200

    New importer - Turner

---
+++ lib/NonameTV/Importer/Turner.pm
@@ -0,0 +1,233 @@
+package NonameTV::Importer::Turner;
+
+use strict;
+use warnings;
+
+=pod
+
+Import data from Word-files delivered via e-mail.  Each day
+is handled as a separate batch.
+
+Features:
+
+=cut
+
+use utf8;
+
+use POSIX;
+use DateTime;
+use XML::LibXML;
+#use Text::Capitalize qw/capitalize_title/;
+
+use NonameTV qw/MyGet Wordfile2Xml Htmlfile2Xml norm AddCategory/;
+use NonameTV::DataStore::Helper;
+use NonameTV::Log qw/info progress error logdie 
+                     log_to_string log_to_string_result/;
+
+use NonameTV::Importer::BaseFile;
+
+use base 'NonameTV::Importer::BaseFile';
+
+sub new {
+  my $proto = shift;
+  my $class = ref($proto) || $proto;
+  my $self  = $class->SUPER::new( @_ );
+  bless ($self, $class);
+
+  $self->{grabber_name} = "Turner";
+
+  my $dsh = NonameTV::DataStore::Helper->new( $self->{datastore} );
+  $self->{datastorehelper} = $dsh;
+
+  return $self;
+}
+
+sub ImportContentFile
+{
+  my $self = shift;
+  my( $file, $chd ) = @_;
+
+  $self->{fileerror} = 0;
+
+  my $xmltvid = $chd->{xmltvid};
+  my $channel_id = $chd->{id};
+  my $dsh = $self->{datastorehelper};
+  my $ds = $self->{datastore};
+  
+  return if( $file !~ /\.doc$/i );
+
+  progress( "Turner: $xmltvid: Processing $file" );
+  
+  my $doc;
+  $doc = Wordfile2Xml( $file );
+
+  if( not defined( $doc ) ) {
+    error( "Turner $xmltvid: $file: Failed to parse" );
+    return;
+  }
+
+  my @nodes = $doc->findnodes( '//span[@style="text-transform:uppercase"]/text()' );
+  foreach my $node (@nodes) {
+    my $str = $node->getData();
+    $node->setData( uc( $str ) );
+  }
+  
+  # Find all paragraphs.
+  my $ns = $doc->find( "//div" );
+  
+  if( $ns->size() == 0 ) {
+    error( "Turner $xmltvid: $file: No divs found." ) ;
+    return;
+  }
+
+  my $currdate = "x";
+  my $date = undef;
+  my @ces;
+  my $description;
+
+  foreach my $div ($ns->get_nodelist) {
+
+    my( $text ) = norm( $div->findvalue( '.' ) );
+
+#print ">$text<\n";
+
+    if( isDate( $text ) ) { # the line with the date in format 'Friday 1st August 2008'
+
+      $date = ParseDate( $text );
+
+      if( $date ) {
+
+        progress("Turner: $xmltvid: Date is $date");
+
+        if( $date ne $currdate ) {
+
+          if( $currdate ne "x" ){
+            # save last day if we have it in memory
+            FlushDayData( $xmltvid, $dsh , @ces );
+            $dsh->EndBatch( 1 );
+          }
+
+          my $batch_id = "${xmltvid}_" . $date;
+          $dsh->StartBatch( $batch_id, $channel_id );
+          $dsh->StartDate( $date , "00:00" ); 
+          $currdate = $date;
+        }
+      }
+
+      # empty last day array
+      undef @ces;
+      undef $description;
+
+    } elsif( isShow( $text ) ) {
+
+      my( $time, $title ) = ParseShow( $text );
+
+      my $ce = {
+        channel_id => $chd->{id},
+        start_time => $time,
+        title => norm($title),
+      };
+
+      # add the programme to the array
+      # as we have to add description later
+      push( @ces , $ce );
+
+    } else {
+
+        # the last element is the one to which
+        # this description belongs to
+        my $element = $ces[$#ces];
+
+        $element->{description} .= $text;
+
+    }
+  }
+
+  # save last day if we have it in memory
+  FlushDayData( $xmltvid, $dsh , @ces );
+
+  $dsh->EndBatch( 1 );
+    
+  return;
+}
+
+sub FlushDayData {
+  my ( $xmltvid, $dsh , @data ) = @_;
+
+    if( @data ){
+      foreach my $element (@data) {
+
+        progress("Turner: $xmltvid: $element->{start_time} - $element->{title}");
+
+        $dsh->AddProgramme( $element );
+      }
+    }
+}
+
+sub isDate {
+  my ( $text ) = @_;
+
+  # format 'Friday 1st August 2008'
+  if( $text =~ /^(monday|tuesday|wednesday|thursday|friday|saturday|sunday)\s+\d+\S*\s+\S+\s+\d+$/i ){
+    return 1;
+  }
+
+  return 0;
+}
+
+sub ParseDate {
+  my( $text ) = @_;
+
+  my( $dayname, $day, $monthname, $year ) = ( $text =~ /^(\S+)\s+(\d+)\S*\s+(\S+)\s+(\d+)$/ );
+
+  my $month = MonthNumber( $monthname , 'en' );
+
+  return sprintf( '%d-%02d-%02d', $year, $month, $day );
+}
+
+sub isShow {
+  my ( $text ) = @_;
+
+  # format 'UK 19.35 / CET 20.35 / CAT 20.35 And the title is here'
+  # or
+  # UK Time 05.30 / CET 06.30 / CAT 06.30 Looney Tunes
+  if( $text =~ /^UK\s+\S*\s*\d+\.\d+\s+\/\s+CET\s+\d+\.\d+\s+\/\s+CAT\s+\d+\.\d+\s+/i ){
+    return 1;
+  }
+
+  return 0;
+}
+
+sub MonthNumber {
+  my( $monthname , $lang ) = @_;
+
+  my @months;
+
+  if( $lang =~ /^en$/ ){
+    @months = qw/january february march april may june july august september october november december/;
+  }
+
+  my %monthnames = ();
+
+  for( my $i = 0; $i < scalar(@months); $i++ )
+    { $monthnames{$months[$i]} = $i+1;}
+
+  my $month = $monthnames{lc $monthname};
+
+  return $month;
+}
+
+sub ParseShow {
+  my( $text ) = @_;
+
+  my( $hour, $min, $title ) = ( $text =~ /^UK\s+\S*\s*\d+\.\d+\s+\/\s+CET\s+(\d+)\.(\d+)\s+\/\s+CAT\s+\d+\.\d+\s+(.*)/ );
+
+  return( $hour . ":" . $min , $title );
+}
+
+1;
+
+### Setup coding system
+## Local Variables:
+## coding: utf-8
+## End:
