ffbf852cc30a4db38f2b1e477900382642d3bcf2
Mattias Holmlund <mattias.git@holmlund.se>
2008-09-13 21:19:32 +0200

    Documentation.

--- lib/NonameTV/Log.pm
+++ lib/NonameTV/Log.pm
@@ -1,82 +1,10 @@
 package NonameTV::Log;
 
-=pod
+=begin nd
+
+package: NonameTV::Log
 
 Logging-module for NonameTV.
-
-=head2 USAGE
-
-The logging functions all take a string as argument. This string is
-prefixed with the chosen severity and current context and appended to
-the different logging outputs that are available.
-
-The following logging functions are available:
-
- d Debug
- p Progress
- w Warning. Execution of this task continued anyway. 
- f Fatal error. Execution of this task will be aborted.
-
-For example:
-
-  if( $errorcondition ) {
-    w "Something wrong";
-  }
-
-  if( $downloadfailed ) {
-    f "Download failed";
-    return 0;
-  }
-
-
-=head3 LogSection
-
-StartLogSection/EndLogSection can be used to catch all warnings and
-fatal errors in a section of code. This is for example used to catch
-log messages emitted for each batch in an importer.
-
- 
-  StartLogSection( "batchname", 1 );
-    (do stuff that may emit log messages)
-  my( $messages, $highestpriority ) = EndLogSection( "batchname" );
-
-
-StartLogSection can be nested. An outer LogSection will NOT catch
-messages that are caught by the inner LogSection.
-
-=head2 LOGGING OUTPUTS
-
-STDERR
-
-Each message is prefixed with the severity and the current LogSection
-name.
-
-Normally, severities Warning and Fatal are printed.
-
---verbose prints Progress as well.
---verbose=2 prints Debug and Progress as well.
---quiet prints only Fatal.
---quiet=2 prints nothing
-
-LOGFILE
-
-LogFile logs everything up to and including severity Progress. Each
-message is prefixed with the severity and the current LogSection name.
-
-LOGSECTION
-
-EndLogSection returns all messages with priority warning and fatal
-that have been issued since the matching call to StartLogSection. The
-messages are prefixed with the severity. In array context,
-EndLogSection returns the messages and an integer describing the
-highest log level used in the section.
-
-Compatibility functions
-
-error
-progress
-
-LogSection is not appended to these strings.
 
 =cut
 
@@ -111,6 +39,47 @@ sub p;
 sub w;
 sub e;
 
+=begin nd
+
+Topic: Severities
+
+Logging is done at four different severities.
+
+ Debug    - For debugging purposes
+ Progress - Something has been updated.
+ Warning  - Something unexpected has happened. 
+            Execution of this task continued anyway. 
+ Fatal    - A fatal error has occured. 
+            Execution of this task will be aborted.
+
+The perl built-in "warn" is overridden by this module and will print a
+message at severity Warning. This means that the message will be
+prefixed by the current LogSection to aid in debugging.
+
+Topic: Log outputs
+
+The log output is sent to three different destinations.
+
+*STDERR*
+
+Each message is prefixed with the severity and the current LogSection
+name. The call to <SetVerbosity> decides which severities are printed
+on STDERR.
+
+
+*LOGFILE*
+
+LogFile logs everything up to and including severity Progress. Each
+message is prefixed with the severity and the current LogSection name.
+
+*LogSection*
+
+EndLogSection returns all messages with severity Warning and Fatal
+that have been issued since the matching call to StartLogSection. The
+messages are prefixed with the severity. 
+
+=cut
+
 use constant {
   DEBUG => 1,
   PROGRESS => 2,
@@ -141,45 +110,106 @@ BEGIN {
   $SIG{'__WARN__'} = \&w;
 }
 
-sub SetVerbosity {
-  my( $verbose, $quiet ) = @_;
+=begin nd
 
-  if( $verbose == 1 ) {
-    $stderr_level = PROGRESS;
-  }
-  elsif( $verbose > 1 ) {
-    $stderr_level = DEBUG;
-  }
-  elsif( $quiet == 1 ) {
-    $stderr_level = FATAL;
-  }
-  elsif( $quiet > 1 ) {
-    $stderr_level = NONE;
-  }
-  else {
-    $stderr_level = WARNING;
-  }
-}
+Group: Logging Functions
 
-sub d {
+The logging functions all take a string as argument. This string is
+prefixed with the chosen severity and current context and appended to
+the different log outputs that are available.
+
+For example:
+
+>  if( $errorcondition ) {
+>    w "Something wrong";
+>  }
+>
+>  if( $downloadfailed ) {
+>    f "Download failed";
+>    return 0;
+>  }
+
+=cut
+
+=begin nd
+
+Print a log-message at severity Debug.
+
+Parameters:
+
+  $message - The message that shall be printed. 
+
+Returns: 
+
+nothing
+
+=cut
+
+sub d #( $message ) 
+{
   my( $message ) = @_;
 
   writelog( DEBUG, "D", $section[0][0], $message );
 }
 
-sub p {
+=begin nd
+
+Print a log-message at severity Progress.
+
+Parameters:
+
+  $message - The message that shall be printed. 
+
+Returns: 
+
+nothing
+
+=cut
+
+sub p #( $message )
+{
   my( $message ) = @_;
 
   writelog( PROGRESS, "P", $section[0][0], $message );
 }
 
-sub w {
+=begin nd
+
+Print a log-message at severity Warning.
+
+Parameters:
+
+  $message - The message that shall be printed. 
+
+Returns: 
+
+nothing
+
+=cut
+
+sub w #( $message )
+{
   my( $message ) = @_;
 
   writelog( WARNING, "W", $section[0][0], $message );
 }
 
-sub f {
+=begin nd
+
+Print a log-message at severity Fatal.
+
+Parameters:
+
+  $message - The message that shall be printed. 
+
+Returns: 
+
+nothing
+
+=cut
+
+sub f #( $message )
+{
   my( $message ) = @_;
 
   writelog( FATAL, "F", $section[0][0], $message );
@@ -221,7 +251,89 @@ sub writelog {
   }
 }
 
-sub StartLogSection {
+=begin nd
+
+Group: Miscellaneous functions
+
+=cut
+
+=begin nd
+
+Set the lowest Severity level that will be printed on STDERR.
+
+Parameters:
+
+  $verbose - Verbosity level, 0, 1 or 2.
+  $quiet - Be quiet, 0 or 1.
+
+Normally, severities Warning and Fatal are printed to STDERR. The SetVerbosity
+function is meant to be used with command-line option parsing where
+each occurence of --verbose increases $verbose with 1 and each
+occurence of --quiet increases $quiet by 1. It gives the following
+result.
+
+  --verbose prints Progress as well.
+  --verbose --verbose prints Debug and Progress as well.
+  --quiet prints only Fatal.
+  --quiet --quiet prints nothing
+
+Note that --quiet only applies to output that is generated within a LogSection where $captured is true. Otherwise, Warning and Fatal are always printed to STDERR. The idea behind this is that --quiet silences output that is captured elsewhere, i.e. in the database somewhere.
+
+=cut
+ 
+sub SetVerbosity #( $verbose, $quiet )
+{
+  my( $verbose, $quiet ) = @_;
+
+  if( $verbose == 1 ) {
+    $stderr_level = PROGRESS;
+  }
+  elsif( $verbose > 1 ) {
+    $stderr_level = DEBUG;
+  }
+  elsif( $quiet == 1 ) {
+    $stderr_level = FATAL;
+  }
+  elsif( $quiet > 1 ) {
+    $stderr_level = NONE;
+  }
+  else {
+    $stderr_level = WARNING;
+  }
+}
+
+=begin nd
+
+  StartLogSection/EndLogSection can be used to catch all warnings and
+  fatal errors in a section of code.
+
+  Parameters:
+    
+    $sectionname - A prefix that will be added to all messages.
+    $captured - False if warnings and fatal messages should be printed 
+                on STDERR even if the verbosity level is set to quiet. 
+                True otherwise.
+
+  Returns:
+
+     Nothing.
+
+This is for example used to catch
+log messages emitted for each batch in an importer.
+
+ 
+>  StartLogSection( "batchname", 1 );
+>    (do stuff that may emit log messages)
+>  my( $messages, $highestpriority ) = EndLogSection( "batchname" );
+
+
+StartLogSection can be nested. An outer LogSection will NOT catch
+messages that are caught by the inner LogSection.
+
+=cut 
+
+sub StartLogSection #( $sectionname, $captured )
+{
   my( $sectionname, $captured ) = @_;
 
   confess "You need to specify $captured to StartLogSection"
@@ -230,7 +342,24 @@ sub StartLogSection {
   unshift @section, [ $sectionname, "", 0, $captured ];
 }
 
-sub EndLogSection {
+
+=begin nd
+
+Parameters:
+  $sectionname - Same name as in the matching call to StartLogSection. 
+    The name is only used to catch bugs where a call to StartLogSection 
+    is not followed by a matching call to EndLogSection. 
+
+Returns:
+
+In array context, EndLogSection returns a string containing the
+messages and an integer describing the highest log level used in the
+section. In scalar context, only the string is returned.
+
+=cut
+
+sub EndLogSection #( $sectionname )
+{
   my( $sectionname ) = @_;
 
   if( $sectionname eq $section[0][0] ) {
@@ -250,14 +379,54 @@ sub EndLogSection {
   }
 }
 
-# Deprecated functions
-sub progress {
+=begin nd
+
+Group: Deprecated functions
+
+These log-functions can be used by older code only. They should not be
+used for new code. The LogSection string is not appended to these
+messages.
+
+=cut
+
+
+=begin nd
+
+Print a log-message at severity Progress.
+
+Parameters:
+
+  $message - The message that shall be printed. 
+
+Returns: 
+
+nothing
+
+=cut
+
+sub progress #( $message )
+{
   my( $message ) = @_;
 
   writelog( PROGRESS, "PROG", undef, $message );
 }
 
-sub error {
+=begin nd
+
+Print a log-message at severity Warning.
+
+Parameters:
+
+  $message - The message that shall be printed. 
+
+Returns: 
+
+nothing
+
+=cut
+
+sub error #( $message )
+{
   my( $message ) = @_;
 
   writelog( WARNING, "ERROR", undef, $message );
