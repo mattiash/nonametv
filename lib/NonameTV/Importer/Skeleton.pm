f0a88ede7bc290fb1f3cab43af42a160b158121f
Mattias Holmlund <mattias.git@holmlund.se>
2008-09-14 21:03:36 +0200

    Use new logging functions.

---
+++ lib/NonameTV/Importer/Skeleton.pm
@@ -0,0 +1,84 @@
+package NonameTV::Importer::Skeleton;
+
+use strict;
+use warnings;
+
+=pod
+
+Sample importer for http-based sources.
+See xxx for instructions. 
+
+=cut
+
+use NonameTV::DataStore::Helper;
+use NonameTV::Log qw/p w f/;
+
+use NonameTV qw/Html2Xml/;
+
+use NonameTV::Importer::BaseWeekly;
+
+use base 'NonameTV::Importer::BaseWeekly';
+
+sub new {
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my $self  = $class->SUPER::new( @_ );
+    bless ($self, $class);
+
+    $self->{grabber_name} = 'Skeleton';
+
+    defined( $self->{UrlRoot} ) or die "You must specify UrlRoot";
+
+    my $dsh = NonameTV::DataStore::Helper->new( $self->{datastore} );
+    $self->{datastorehelper} = $dsh;
+
+    return $self;
+}
+
+sub Object2Url {
+  my $self = shift;
+  my( $objectname, $chd ) = @_;
+
+  my( $year, $week ) = ( $objectname =~ /(\d+)-(\d+)$/ );
+ 
+  my $url = $self->{UrlRoot} . "/" . $chd->{grabber_info} . "/" . 
+      "$year-$week.html";
+
+  # Only one url to look at and no error
+  return ([$url], undef);
+}
+
+sub FilterContent {
+  my $self = shift;
+  my( $cref, $chd ) = @_;
+
+  # Convert the html into xml
+  my $tree = Html2Xml( $$cref );
+  my $str = $tree->toString(1);
+
+  return( \$str, undef );
+}
+
+sub ContentExtension {
+  return 'html';
+}
+
+sub FilteredExtension {
+  return 'xml';
+}
+
+sub ImportContent {
+  my $self = shift;
+
+  my( $batch_id, $cref, $chd ) = @_;
+
+  my $ds = $self->{datastore};
+  my $dsh = $self->{datastorehelper};
+
+  my $tree = ParseXml( $cref );
+
+  return;
+}
+
+
+1;
